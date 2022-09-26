e8e87d7f013594b5d74b0f8974e0c9250be30861
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2022-09-26 02:29:05 -0700

    delete double

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get update;
 sudo apt-get dist-upgrade -y;
 sudo apt-get autoclean -y;
 sudo apt-get autoremove -y;
-sudo apt-get install apache2
+sudo apt-get install apache2;
 sudo snap install core;
 sudo snap refresh core;
 sudo snap install certbot --classic;
