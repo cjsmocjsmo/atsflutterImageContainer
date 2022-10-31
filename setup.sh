e39da225b5c5662372034d1223d9350adf54b5d0
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2022-10-31 09:48:33 -0700

    added privacy policy

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-sudo apt-get update;
-sudo apt-get dist-upgrade -y;
-sudo apt-get autoclean -y;
-sudo apt-get autoremove -y;
-sudo apt-get install apache2;
+sudo apt-get update && \
+sudo apt-get dist-upgrade -y && \
+sudo apt-get autoclean -y && \
+sudo apt-get autoremove -y && \
+sudo apt-get install apache2 && \
 sudo snap install core && \
 sudo snap refresh core && \
 sudo snap install certbot --classic && \
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot --apache;
-sudo rm /var/www/html/index.html
+# sudo certbot --apache;
+# sudo rm /var/www/html/index.html
 
 sudo a2enmod headers
 
